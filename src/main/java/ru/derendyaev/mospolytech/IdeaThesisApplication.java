6b250209bd42edc9fc6171e7e124046805df94fb
Dmitrii Derendyaev <dds_stalker@mail.ru>
2024-10-06 11:55:16 +0400

    init

---
+++ src/main/java/ru/derendyaev/mospolytech/IdeaThesisApplication.java
@@ -0,0 +1,13 @@
+package ru.derendyaev.mospolytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaThesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaThesisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/derendyaev/mospolytech/IdeaThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.derendyaev.mospolytech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
