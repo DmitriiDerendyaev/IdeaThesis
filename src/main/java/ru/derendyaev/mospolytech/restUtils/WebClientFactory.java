967419e761cec3190b9a788a4762893e4270b5ea
Dmitrii Derendyaev <dds_stalker@mail.ru>
2024-10-06 22:32:12 +0400

    feature: игнорировать SSL подключение

--- src/main/java/ru/derendyaev/mospolytech/restUtils/WebClientFactory.java
+++ src/main/java/ru/derendyaev/mospolytech/restUtils/WebClientFactory.java
@@ -1,6 +1,9 @@
 package ru.derendyaev.mospolytech.restUtils;
 
 import io.netty.channel.ChannelOption;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
@@ -9,24 +12,30 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.TcpClient;
 
+import javax.net.ssl.SSLException;
 import java.util.concurrent.TimeUnit;
 
 @Component
-public abstract class WebClientFactory {
+public class WebClientFactory {
     private static final int TIMEOUT = 1000;
 
-    public WebClient createWebClient(String baseUrl) {
-        final var tcpClient = TcpClient
-                .create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
-                .doOnConnected(connection -> {
-                    connection.addHandlerLast(new ReadTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
-                    connection.addHandlerLast(new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
-                });
+    public static WebClient createWebClient(String baseUrl) {
+        SslContext sslContext = null;
+        try {
+            sslContext = SslContextBuilder
+                    .forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                    .build();
+        } catch (SSLException e) {
+            throw new RuntimeException(e);
+        }
+
+        SslContext finalSslContext = sslContext;
+        HttpClient httpClient = HttpClient.create().secure(t -> t.sslContext(finalSslContext));
 
         return WebClient.builder()
                 .baseUrl(baseUrl)
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
     }
 }
